de0b87d29739af9d1199b5845361f70e0ad6bb74
kainat999 <kainatbajwah999@gmail.com>
2023-09-23 03:31:26 +0500

    typo

--- testapp/home/serializer.py
+++ testapp/home/serializer.py
@@ -1,41 +1,24 @@
-# from rest_framework import serializers
-# from .models import Todo
-# import re
-
-# class TodoSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Todo
-#       #  fields = '__all__'
-#         fields = ['todo_title', 'todo_description', 'is_done']
-#         # exclude = ['created_at', 'updated_at', 'u_id']
-
-
-#         def vlidate(self, validated_data):
-#             if validated_data.get('todo_title'):
-                
-#                 todo_title = validated_data['todo_title']  
-#                 regex = re.compile(r'[~!@#$%^&*()_+|}{\][?><,./]')
-                
-#                 if not regex.search(todo_title) == None:
-#                   raise serializers.ValidationError('Todo title cannot contain specail charactors!')
-#             return validated_data     
-
-
 from rest_framework import serializers
-from django.core.validators import RegexValidator
 from .models import Todo
+import re
 
 class TodoSerializer(serializers.ModelSerializer):
-    todo_title = serializers.CharField(
-        validators=[
-            RegexValidator(
-                regex=r'^[a-zA-Z0-9\s]*$',
-                message='Todo title cannot contain special characters!',
-                code='invalid_title'
-            )
-        ]
-    )
-
     class Meta:
         model = Todo
+      #  fields = '__all__'
         fields = ['todo_title', 'todo_description', 'is_done']
+        # exclude = ['created_at', 'updated_at', 'u_id']
+
+
+        def validate(self, validated_data):
+            if validated_data.get('todo_title'):
+                
+                todo_title = validated_data['todo_title']  
+                regex=r'^[a-zA-Z0-9\s]*$'
+                
+                if not regex.search(todo_title) == None:
+                  raise serializers.ValidationError('Todo title cannot contain specail charactors!')
+            return validated_data     
+
+
+# 
