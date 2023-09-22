3aaa133af158c28d8981228e1e8cdc5496ccfc40
kainat999 <kainatbajwah999@gmail.com>
2023-09-23 02:44:31 +0500

    updation

--- testapp/home/serializer.py
+++ testapp/home/serializer.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 from .models import Todo
-
+import re
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
       #  fields = '__all__'
-        # fields = ['todo_title']
-        exclude = ['created_at', 'updated_at', 'u_id']
+        fields = ['todo_title', 'todo_description', 'is_done']
+        # exclude = ['created_at', 'updated_at', 'u_id']
+
+
+        def velidate(self, validated_data):
+            if validated_data.get('todo_title'):
+                
+                todo_title = validated_data['todo_title']  
+                regex = re.compile(r'[~!@#$%^&*()_+|}{\][?><,./]')
+                
+                if not regex.search(todo_title) == None:
+                  raise serializers.ValidationError('Todo title cannot contain specail charactors!')
+            return validated_data     
+
+
